3ccb85e3228636c87a8b55429ff72fdbe3912cd4
azhang168 <146869774+azhang168@users.noreply.github.com>
2023-11-24 16:02:32 -0700

    Update system_functions.py

--- system_functions.py
+++ system_functions.py
@@ -1,4 +1,5 @@
 from pymongo.collation import Collation
+from datetime import datetime
 
 def search_tweets(tweetscollection):
    
@@ -49,10 +50,160 @@ def list_tweets(tweetscollection):
     pass
 
 def list_users(tweetscollection):
-    pass
+    # This function displays the top n users based on followersCount (n is inputted by the user)
+
+    length = tweetscollection.count_documents({"_id": {"$exists":True}})
+    # prompt user for number of accounts they would like to see
+    n = input("Enter the number of accounts you would like to see: ")
+    # check if the input is valid
+    while True:
+        # check if an integer is inputted
+        try:
+            n = int(n)
+        except Exception:
+            print("Invalid input.")
+            n = input("Enter the number of accounts you would like to see: ")
+            continue
+        # check if the integers is within range of 0 to the number of documents
+        if n > length or n < 0:
+            print("Invalid input.")
+            n = input("Enter the number of accounts you would like to see: ")
+        else:
+            break
+    # sort the documents by followersCount
+    results = tweetscollection.find().sort("user.followersCount", -1)
+    shown = []  # keep track of possible duplicates
+    i = 0
+    limit = 0
+    # display top n accounts
+    while limit < n and i < length:
+        if results[i]["user"]["username"].lower() not in shown:
+            shown.append(results[i]["user"]["username"].lower())
+            print("Username: %s" % results[i]["user"]["username"])
+            print("Display name: %s" % results[i]["user"]["displayname"])
+            print("Followers count: %d" % results[i]["user"]["followersCount"])
+            print("")
+            limit += 1
+        i += 1
+    # prompt user for the username of an account they would like to see more information about
+    select_user = input("Enter a username to see more information: ").lower()
+    validUser = False
+    while not validUser:
+        for username in shown:
+            if select_user == username:
+                validUser = True
+                break
+        if validUser:
+            break
+        else:
+            print("Invalid username.")
+            select_user = input("Enter a username to see more information: ").lower()
+    # display all user information
+    for acc in results:
+        if acc["user"]["username"].lower() == select_user:
+            print("Username: %s" % acc["user"]["username"])
+            if acc["user"]["displayname"]:
+                print("Display name: %s" % acc["user"]["displayname"])
+            if acc["user"]["id"]:
+                print("ID: %d" % acc["user"]["id"])
+            if acc["user"]["description"]:
+                print("Description: %s" % acc["user"]["description"])
+            if acc["user"]["verified"]:
+                print("Verified")
+            else:
+                print("Not verified")
+            if acc["user"]["created"]:
+                print("Date created: %s" % acc["user"]["created"])
+            if acc["user"]["followersCount"]:
+                print("Followers count: %d" % acc["user"]["followersCount"])
+            if acc["user"]["friendsCount"]:
+                print("Friends count: %d" % acc["user"]["friendsCount"])
+            if acc["user"]["statusesCount"]:
+                print("Statuses count: %d" % acc["user"]["statusesCount"])
+            if acc["user"]["favouritesCount"]:
+                print("Favourites count: %d" % acc["user"]["favouritesCount"])
+            if acc["user"]["listedCount"]:
+                print("Listed count: %d" % acc["user"]["listedCount"])
+            if acc["user"]["mediaCount"]:
+                print("Media count: %d" % acc["user"]["mediaCount"])
+            if acc["user"]["location"] != "":
+                print("Location: %s" % acc["user"]["location"])
+            if acc["user"]["protected"]:
+                print("Protected")
+            if acc["user"]["linkUrl"]:
+                print("Link URL: %s" % acc["user"]["linkUrl"])
+            if acc["user"]["linkTcourl"]:
+                print("Link t.co URL: %s" % acc["user"]["linkTcourl"])
+            if acc["user"]["profileImageUrl"]:
+                print("Profile Image URL: %s" % acc["user"]["profileImageUrl"])
+            if acc["user"]["profileBannerUrl"]:
+                print("Profile Banner URL: %s" % acc["user"]["profileBannerUrl"])
+            if acc["user"]["url"]:
+                print("URL: %s" % acc["user"]["url"])
+    return
 
 def compose_tweet(tweetscollection):
-    pass
+    # This function allows the user to create a tweet and inserts the tweet into the database
+
+    # prompt the user to input content
+    content = input("Enter your tweet: ")
+    # get system date and time
+    date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S+00:00")
+    # create the new tweet document
+    new_tweet = {
+    "url": None,
+    "date": date,
+    "content": content,
+    "renderedContent": content,
+    "id": None,
+    "user": {
+        "username": "291user",
+        "displayname": None,
+        "id": None,
+        "description": None,
+        "rawDescription": None,
+        "descriptionUrls": [],
+        "verified": False,
+        "created": None,
+        "followersCount": 0,
+        "friendsCount": 0,
+        "statusesCount": 0,
+        "favouritesCount": 0,
+        "listedCount": 0,
+        "mediaCount": 0,
+        "location": "",
+        "protected": False,
+        "linkUrl": None,
+        "linkTcourl": None,
+        "profileImageUrl": None,
+        "profileBannerUrl": None,
+        "url": None
+    },
+    "outlinks": [],
+    "tcooutlinks": [],
+    "replyCount": 0,
+    "retweetCount": 0,
+    "likeCount": 0,
+    "quoteCount": 0,
+    "conversationId": None,
+    "lang": None,
+    "source": None,
+    "sourceUrl": None,
+    "sourceLabel": None,
+    "media": [
+        {
+        "previewUrl": None,
+        "fullUrl": None,
+        "type": None
+        }
+    ],
+    "retweetedTweet": None,
+    "quotedTweet": None,
+    "mentionedUsers": None
+    }
+    # insert into database
+    tweetscollection.insert_one(new_tweet)
+    return
 
 
 def check_tweet_select(list_items, user_input):
